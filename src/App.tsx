23df2614de7c1bc61478bc036f2de20a94022b90
djli15736 <djli15736@gmail.com>
2021-09-22 13:13:54 -0400

    install bootstrap, use containers instead of divs

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import './App.css';
 import QUOTES from './assets/quotes.json';
 import { ControlPanel } from './components/ControlPanel';
 import { CardViewer } from './components/QuoteViewer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row } from 'react-bootstrap';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <ControlPanel></ControlPanel>
-      <CardViewer></CardViewer>
-    </div>
+    <Container className="App">
+      <Row>
+        <ControlPanel></ControlPanel>
+        <CardViewer></CardViewer>
+      </Row>
+    </Container>
   );
 }
 
