4616d17b523bf936646b553fe2a076dd19d33a79
djli15736 <djli15736@gmail.com>
2021-09-22 13:54:26 -0400

    getRandomElement created

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -1,10 +1,14 @@
 import { Button, Col } from 'react-bootstrap';
 import { Quote } from '../interfaces/quote';
+import QUOTES from '../assets/quotes.json';
+import { getRandomElement } from '../utilities/data';
 
 
 export function ControlPanel({setQuote}: {setQuote: (q: Quote)=>void}): JSX.Element {
     return <Col>
     <h1> Control Panel </h1>
-    <Button>Swap Current Quote</Button>
+    <Button onClick={
+        () => setQuote(getRandomElement(QUOTES as Quote[]))
+    }>Swap Current Quote</Button>
     </Col>
 }
