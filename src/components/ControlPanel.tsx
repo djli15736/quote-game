df7bd833341b08fe359d85720c8340cc9b884838
djli15736 <djli15736@gmail.com>
2021-09-22 20:18:27 -0400

    save button

--- src/components/ControlPanel.tsx
+++ src/components/ControlPanel.tsx
@@ -6,15 +6,28 @@ import { UserList } from './UserList';
 import { useState } from 'react';
 import { Task as User } from 'editable-dnd-list';
 
+export const LOCAL_STORAGE_USERS = 'quote-game-users';
+
+export const INITIAL_USERS: User[] = [
+    {id: '1', text: "Daniel"},
+    {id: '2', text: "Jim"},
+    {id: '3', text: "Bob"},
+    {id: '4', text: "Michael"},
+    {id: '5', text: "Tony"}
+];
+
+export function getLocalStorageUsers(): User[] {
+    let rawUsers: string|null = localStorage.getItem(LOCAL_STORAGE_USERS);
+    if (rawUsers === null){
+        return [...INITIAL_USERS]
+    } else {
+        return JSON.parse(rawUsers);
+    }
+}
 
 export function ControlPanel({setQuote, reveal, sourceRevealed}: {setQuote: (q: Quote)=>void, reveal: (r: boolean)=> void, sourceRevealed: boolean}): JSX.Element {
-    const [users, setUsers] = useState<User[]>([
-        {id: '1', text: "Daniel"},
-        {id: '2', text: "Jim"},
-        {id: '3', text: "Bob"},
-        {id: '4', text: "Michael"},
-        {id: '5', text: "Tony"}
-    ]);
+    
+    const [users, setUsers] = useState<User[]>(getLocalStorageUsers);
     
     function setRandomCard() {
         reveal(false);
@@ -26,11 +39,16 @@ export function ControlPanel({setQuote, reveal, sourceRevealed}: {setQuote: (q: 
         setUsers([...shuffledUsers]);
     }
 
+    function save(){
+        localStorage.setItem(LOCAL_STORAGE_USERS, JSON.stringify(users));
+    }
+
     return <Col>
     <h1> Control Panel </h1>
     <UserList users={users} setUsers={setUsers}></UserList>
     <Button onClick={setRandomCard} className="m-4">Swap Current Quote</Button>
     <Button onClick={() => reveal(!sourceRevealed)} className="m-4"> Reveal Source </Button>
     <Button onClick={shuffleUsers} className="m-4"> Shuffle Users</Button>
+    <Button onClick={save} className="m-4" variant="success">Save</Button>
     </Col>
 }
